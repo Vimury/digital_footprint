f4343d4dcd90415e5b1ecf7b3d87cc846da24e9c
IAmiron <geortechov375@gmail.com>
2023-03-25 20:11:54 +0300

    Страница вывода студентов

--- data/student_class.py
+++ data/student_class.py
@@ -1,6 +1,10 @@
 import datetime
 
 import sqlalchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, DateField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
 
 from data.db_session import SqlAlchemyBase
 
@@ -11,6 +15,14 @@ class Student(SqlAlchemyBase):
     id_student = sqlalchemy.Column(sqlalchemy.Integer,
                                    primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String)
-    birthday = sqlalchemy.Column(sqlalchemy.DateTime,
-                                 default=datetime.datetime.now)
-    id_stepik = None
+    birthday = sqlalchemy.Column(sqlalchemy.String,
+                                 default=str(datetime.datetime.now))
+    id_stepik = sqlalchemy.Column(sqlalchemy.String,
+                                 default="НЕТУ")
+
+
+class StudentForm(FlaskForm):
+    name = StringField('ФИО', validators=[DataRequired()])
+    date = StringField("Дата рождения", validators=[DataRequired()])
+    id_stepik = StringField('ID Stepik', validators=[DataRequired()])
+    submit = SubmitField('Применить')
